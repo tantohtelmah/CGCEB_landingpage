0f158ce3f39e767c9dd95c808cb1151c5fe13fe6
tantohtelmah <tantohtelmah@gmail.com>
2024-06-12 00:45:43 -0400

    more pages

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,37 @@ window.addEventListener("scroll", function() {
         header.classList.remove("bg-white", "shadow-md");
     }
 });
+
+function validate_password() {
+    const password = document.getElementById("password").value;
+    const retypePassword = document.getElementById("retype_password").value;
+
+    if (password === retypePassword) {
+        return true; // Allow form submission
+    } else {
+        alert("Passwords do not match!");
+        return false; // Prevent form submission
+    }
+}
+
+function validate_credentials() {
+    const password = document.getElementById("gceid").value;
+    const retypePassword = document.getElementById("pwd").value;
+
+    if (password === retypePassword) {
+        return true; // Allow form submission
+    } else {
+        alert("Passwords do not match!");
+        return false; // Prevent form submission
+    }
+}
+
+function credential_check() {
+    if (validate_password()) {
+        alert("Sign in successful!")
+        location.replace("trial.html");
+            } else {
+                alert("Invalid credentials. Please try again.");
+                return false;
+    }
+}
